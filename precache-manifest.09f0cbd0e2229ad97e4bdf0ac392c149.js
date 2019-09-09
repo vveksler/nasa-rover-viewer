bdd7c18d678c070cd76ce5e5d6c7f4a968aeb47c
Vadim <vbulka1986@gmail.com>
2019-09-09 18:10:33 +0300

    gh pages

---
+++ precache-manifest.09f0cbd0e2229ad97e4bdf0ac392c149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63e40655ce353ad91f75cb6bad377b5c",
+    "url": "/static/media/logo.63e40655.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d907a52b4585fe31ccbc",
+    "url": "/static/js/main.d907a52b.chunk.js"
+  },
+  {
+    "revision": "d49d5e57c1e7e579301b",
+    "url": "/static/js/1.d49d5e57.chunk.js"
+  },
+  {
+    "revision": "d907a52b4585fe31ccbc",
+    "url": "/static/css/main.c3b6f33a.chunk.css"
+  },
+  {
+    "revision": "6c5150ebbbe7e2b5d42bfedc86c34a5d",
+    "url": "/index.html"
+  }
+];
